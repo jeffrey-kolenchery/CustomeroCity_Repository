4ddb8d57460148f0246d5a83191897723fd561ff
jeffrey-kolenchery <jeffrey.kolenchery@gmail.com>
2021-08-23 17:47:54 +0400

    added mongodb connection -  server connection possible

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 app.use(cors());
 
 //Jeffrey: Putting my personal address in here, later on need to change it to a more general login ie api123 or smth
-const CONNECTION_URL = "mongodb+srv://jeffrey_k:lyoZVWj23yn8fleN@comp30022.5hadw.mongodb.net/test?authSource=admin&replicaSet=atlas-eu6xvh-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+const CONNECTION_URL = "mongodb+srv://jeffrey_k:ndPCUoScKPtC4bju@comp30022.5hadw.mongodb.net/test?authSource=admin&replicaSet=atlas-eu6xvh-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
