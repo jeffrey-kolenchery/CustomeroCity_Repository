8cd59d0337f9d0354293a2af028ba4313fe7a972
taegyuy <tgyyun@gmail.com>
2021-09-02 12:33:30 +1000

    save jwt token to cookie, implement log out functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,63 +1,74 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const register  = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, 
-        function(err, hashedPass) {
-            if(err) {
-                res.status(403).send("Forbidden");
-            }
-            let user = new User ({
-                givenName: req.body.givenName,
-                email: req.body.email,
-                phone: req.body.phone,
-                password: hashedPass
-            })
-            user.save()
-                .then(user => {
-                    res.status(200).send('User Added Successfully!');
-                })
-            .catch(error=> {
-                res.status(400).send('An error occured!');
-            })
-        }
-    )
+const register = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, function (err, hashedPass) {
+    if (err) {
+      res.status(403).send("Forbidden");
+    }
+    let user = new User({
+      givenName: req.body.givenName,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: hashedPass,
+    });
+    user
+      .save()
+      .then((user) => {
+        res.status(200).send("User Added Successfully!");
+      })
+      .catch((error) => {
+        res.status(400).send("An error occured!");
+      });
+  });
 };
 const login = (req, res, next) => {
-    var username = req.body.username;
-    var password = req.body.password;
+  var username = req.body.username;
+  var password = req.body.password;
 
-    User.findOne({$or: [{email:username}, {phone: username}]})
-    .then(user => {
-        if(user){
-            bcrypt.compare(password, user.password, function(err, result) {
-                if(err) {
-                    res.json({
-                        error: err
-                    })
-                }
-                if(result) {
-                    let token = jwt.sign({name: user.name}, 'verysecretValue', {expiresIn: '1hr'})
-                    res.json({
-                        message: 'Login Successful!', 
-                        token
-                    })
-                }else {
-                    res.json({
-                        message: 'Password does not match!', 
-                    })
-                }
-            })
-        } else{
+  User.findOne({ $or: [{ email: username }, { phone: username }] }).then(
+    (user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, function (err, result) {
+          if (err) {
             res.json({
-                message: 'No user found'
-            })
-        }
-    })
+              error: err,
+            });
+          }
+          if (result) {
+            let token = jwt.sign({ name: user.name }, "verysecretValue", {
+              expiresIn: "1hr",
+            });
+            // Persist the token as 't' in cookie with expiry date
+            res.cookie("t", token, { expire: new Date() + 9999 });
+            res.json({
+              message: "Login Successful!",
+              token,
+            });
+          } else {
+            res.json({
+              message: "Password does not match!",
+            });
+          }
+        });
+      } else {
+        res.json({
+          message: "No user found",
+        });
+      }
+    }
+  );
+};
+
+const signout = (req, res) => {
+  // Clear token cookie.
+  res.clearCookie("t");
+  res.json({ message: "Signout success" });
 };
 
 module.exports = {
-    register,
-    login
+  register,
+  login,
+  signout,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const userRouter  = express.Router();
+const express = require("express");
+const userRouter = express.Router();
 
-const UserController = require('../controllers/UserController');
+const UserController = require("../controllers/UserController");
+const AuthController = require("../controllers/authController");
 
-userRouter.post('/register', UserController.register);
-userRouter.post('/login', UserController.login);
+userRouter.post("/register", UserController.register);
+userRouter.post("/login", UserController.login);
 
 module.exports = userRouter;
