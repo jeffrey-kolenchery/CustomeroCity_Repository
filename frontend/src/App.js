c05b6680c26db56507457b0197b32a860f685abe
tsiby <thomas.siby6104@gmail.com>
2021-09-21 18:00:47 +0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,7 @@ import Login from "./views/Login";
 function App() {
   return (
     <div>
-      {/* Route components are rendered if the path prop matches the current URL */}
-      <Switch>
-        {/* Vendor Routes */}
-        {/* <Route path="/register" component={Register} /> */}
-        <Route path="/login" component={Login} />
-      </Switch>
+      <Route path="/login" component={Login} />
     </div>
   );
 }
