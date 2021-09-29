1746f44fe3195b5e040ddf0ef647eadfb6c48e21
jeffrey-kolenchery <jeffrey.kolenchery@gmail.com>
2021-09-29 12:38:02 +0400

    testing lint feature

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,28 +3,28 @@ import mongoose from 'mongoose'
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-  givenName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-    set: (email) => email.toLowerCase(),
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  resetToken: String,
-  expireToken: Date,
+    givenName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        set: (email) => email.toLowerCase(),
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    resetToken: String,
+    expireToken: Date,
 })
 
 const User = mongoose.model('users', userSchema)
