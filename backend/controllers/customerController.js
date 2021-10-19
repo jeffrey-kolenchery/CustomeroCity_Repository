f1ce905be0ff5b5d107496a2001ca306d6eecac9
sanskarb235 <69449563+sanskarb235@users.noreply.github.com>
2021-10-19 17:49:51 +0530

    Changes to backend controllers

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -2,30 +2,22 @@
 // const Customer = require("../models/customerModel");
 import * as bcrypt from 'bcrypt'
 import * as jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
 import { Customer } from '../models/customerModel.js'
 
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
 
-const searchCustomers = async (req, res) => {
-    try {
-        const customers = await Customer.find({ user: req.profile._id })
-        res.send(customers)
-    } catch (err) {
-        res.error(400)
-    }
-}
 
 const registerCustomer = (req, res, next) => {
     const customer = new Customer({
         givenName: req.body.givenName,
         designation: req.body.designation,
-        company: req.body.comapany,
+        company: req.body.company,
         email: req.body.email,
         age: req.body.age,
-        phoneNo: req.body.phoneNo,
+        phoneNo: req.body.phone,
         interests: req.body.interests,
-        user: req.profile._id,
+        user: req.params.userId,
         visitCount : 0,
     })
     customer
@@ -36,6 +28,91 @@ const registerCustomer = (req, res, next) => {
         .catch((error) => {
             res.status(400).send('An error occured!')
         })
+}
+
+const returnCustomer = async (req, res) => {
+    try {
+        const customer = await Customer.find({ _id : new mongoose.Types.ObjectId(req.body.customerId)})
+        res.send(customer)
+    } catch (err) {
+        res.error(400)
+    }
+}
+
+const searchCustomers = async (req,res) => {
+    try {
+        const customers = await Customer.aggregate(
+            [
+                {
+                    '$search': 
+                    {
+                        'index': 'Customer Search', 
+                        'text': 
+                            {
+                                'query': req.body.searchText, 
+                                'path': 'givenName'
+                            }
+                    }
+                }, 
+                {
+                    '$project': 
+                    {
+                        '_id': 1,
+                        'givenName': 1, 
+                        'email': 1, 
+                        'company': 1,
+                        'designation' : 1, 
+                        'user': 1,
+                    }
+                }, 
+                {
+                    '$match': 
+                    {
+                        // 'user': req.params.userId
+                        'user': new mongoose.Types.ObjectId(req.params.userId)
+
+                    }
+                }
+            ]
+        )
+        if (customers) {
+            res.json(customers).status(200)
+        }
+        else res.status(404).send('No customers found')
+    }
+    catch (err) {
+        res.error(400)
+    }
+}
+
+const customerData = async (req, res) => {
+    // const userID = req.params.userId
+    // console.log(userID)
+    try {
+        const customers = await Customer.aggregate(
+            [
+                {
+                    '$match': {
+                        'user':  new mongoose.Types.ObjectId(req.params.userId)
+                    }
+                }, {
+                    '$project': {
+                        'givenName': 1, 
+                        'email': 1, 
+                        'company': 1, 
+                        'designation': 1, 
+                        '_id': 1
+                    }
+                }
+            ]
+        )
+        if (customers) {
+            res.status(200).json(customers)
+        }
+    }
+    catch(error) {
+        res.send('No customers exist. Add some customers!').status(404)
+    }
 }
 
 const deleteCustomer = (req, res, next) => {
@@ -89,7 +166,9 @@ const editCustomer = (req,res,next) => {
 
 export {
     registerCustomer, 
-    deleteCustomer, 
+    deleteCustomer,
+    returnCustomer, 
     searchCustomers, 
-    editCustomer
+    editCustomer,
+    customerData
 }
