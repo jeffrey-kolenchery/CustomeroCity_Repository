73a88d7089e1d78d058e1a695aceba7278fae0b7
sanskarb235 <69449563+sanskarb235@users.noreply.github.com>
2021-10-20 16:06:29 +0530

    Logout feature implemented

--- frontend/src/components/Profile/Sidebar.jsx
+++ frontend/src/components/Profile/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { userView } from '../../api'
+import { userView, userSignOut} from '../../api'
 
 const Sidebar = () => {
     return (
@@ -77,7 +77,10 @@ const Sidebar = () => {
                                         <path fillRule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
                                         <path fillRule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
                                     </svg>
-                                    <Link to="/Logout">
+                                    <Link to="/" onClick={(e) => {
+                                        userSignOut()
+                                        window.sessionStorage.clear()
+                                    }}>
                                         <span className="text-sm ml-2 text-black hover:text-pink-600 cursor-pointer font-mono">Logout</span>
                                     </Link>
                                 </a>
